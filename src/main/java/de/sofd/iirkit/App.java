3d2a38dee07451555b26639a6de2bb3b746295a9
Olaf Klischat <olaf.klischat@sofd.de>
2010-11-22 06:04:36 +0100

    hsql, rhino js dependencies; nbactions.xml for running/debugging with working System.in

--- src/main/java/de/sofd/iirkit/App.java
+++ src/main/java/de/sofd/iirkit/App.java
@@ -1,13 +1,18 @@
 package de.sofd.iirkit;
 
+import org.hsqldb.jdbcDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
+        org.hsqldb.jdbcDriver dr = new jdbcDriver();
         System.out.println( "Hello World!" );
+        org.mozilla.javascript.tools.shell.Main.main(new String[0]);
+
+        System.out.println("DONE.");
     }
 }
