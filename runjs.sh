031e0b0938a2dddbbb61ae121dc624f5384e867f
Olaf Klischat <olaf.klischat@sofd.de>
2011-01-14 18:18:16 +0100

    runjs update: include d.s.{util,viskit} into classpath

--- runjs.sh
+++ runjs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "build/classes:build/test/classes:`echo lib/*jar | tr ' ' ':'`" org.mozilla.javascript.tools.shell.Main
+java -cp "build/classes:build/test/classes:../de.sofd.util/build/classes:../de.sofd.viskit/build/classes:`echo lib/*jar | tr ' ' ':'`" org.mozilla.javascript.tools.shell.Main
