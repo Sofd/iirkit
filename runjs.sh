ba803ed122974b4fdf3865497ee987b75a36cf01
Olaf Klischat <olaf.klischat@sofd.de>
2011-01-14 20:59:30 +0100

    runjs update: include lib/ subdirectories (e.g. JOGL) into classpath

--- runjs.sh
+++ runjs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "build/classes:build/test/classes:../de.sofd.util/build/classes:../de.sofd.viskit/build/classes:`echo lib/*jar | tr ' ' ':'`" org.mozilla.javascript.tools.shell.Main
+java -cp "build/classes:build/test/classes:../de.sofd.util/build/classes:../de.sofd.viskit/build/classes:`find lib/ -name '*jar' -print0 | tr '\0' ':'`" org.mozilla.javascript.tools.shell.Main
