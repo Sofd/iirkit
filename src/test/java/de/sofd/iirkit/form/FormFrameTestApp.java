adc4556e5e3ce5ec5d9375e56cabbb36a466cb6a
Olaf Klischat <olaf.klischat@sofd.de>
2012-04-23 00:44:39 +0200

    FormFrameTestApp extended (control window)

--- src/test/java/de/sofd/iirkit/form/FormFrameTestApp.java
+++ src/test/java/de/sofd/iirkit/form/FormFrameTestApp.java
@@ -1,6 +1,9 @@
 package de.sofd.iirkit.form;
 
 import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QMainWindow;
+import com.trolltech.qt.gui.QToolBar;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -8,9 +11,35 @@ import com.trolltech.qt.gui.QApplication;
  */
 public class FormFrameTestApp {
 
+    private static final Logger logger = Logger.getLogger(FormFrameTestApp.class);
+
+    FormFrame formFrame;
+
     public FormFrameTestApp(String[] args) {
-        FormFrame formFrame = new FormFrame("file:///home/olaf/hieronymusr/iirkit-test/ecrf/312046_11.html", null);
+        formFrame = new FormFrame("file:///home/olaf/hieronymusr/iirkit-test/ecrf/312046_11.html", null);
+        formFrame.setFormDoneCallback(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("FORM DONE CALLBACK. isFormSubmitted: " + formFrame.getFormDoneEvent().isFormSubmitted());
+            }
+        });
         formFrame.show();
+
+        QMainWindow controllerFrame = new QMainWindow();
+        controllerFrame.setWindowTitle("control");
+        QToolBar toolbar = controllerFrame.addToolBar("Actions");
+        toolbar.setFloatable(false);
+        toolbar.addAction("show").triggered.connect(this, "showForm()");
+        toolbar.addAction("close").triggered.connect(this, "closeForm()");
+        controllerFrame.show();
+    }
+
+    private void showForm() {
+        formFrame.show();
+    }
+
+    private void closeForm() {
+        formFrame.close();
     }
 
     public static void main(final String[] args) throws Exception {
